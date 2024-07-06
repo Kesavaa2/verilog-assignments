f5601afc310b3c92d9890b603b672cec005608b9
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-06 14:50:45 +0530

    Update glitch_free_mux.v

--- assignment 1 /glitch_free_mux.v
+++ assignment 1 /glitch_free_mux.v
@@ -1,1 +1,14 @@
-
+module glitch_free_mux (
+    input clk_62_5mhz,
+    input clk_250mhz,
+    input [1:0] rate,
+    output reg clk_out
+);
+    always @(*) begin
+        case (rate)
+            2'b00: clk_out = clk_62_5mhz;
+            2'b10: clk_out = clk_250mhz;
+            default: clk_out = clk_62_5mhz; 
+        endcase
+    end
+endmodule
